767d428b0a6b85ab14cd98e8ce13519f4658347e
amessd <ame.ssd001@gmail.com>
2024-03-18 20:00:40 +0800

    added to gradle & manifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()  // Add this line if it's not already there
+    }
+    dependencies {
+        // Add this line
+        classpath ("com.google.gms:google-services:4.4.1")
+    }
+}
 plugins {
     id("com.android.application") version "8.2.1" apply false
 }
