eb1c98f6dda3944b2b8c60cdb420b002e22de89a
Pranav <psharma@theopenvaultatocbc.com>
2024-03-18 20:53:59 +0800

    created MainActivity.java file

--- app/src/main/java/com/example/binbolehxfirebase/MainActivity.java
+++ app/src/main/java/com/example/binbolehxfirebase/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.binbolehxfirebase;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add Firebase initialization code here
+        FirebaseApp.initializeApp(this);
+    }
+}
 
